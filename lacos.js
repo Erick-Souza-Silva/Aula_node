7844e2adb8707ea0622c7d1035583961a18a36c8
Erick de souza <ericsouzasilv@gmail.com>
2026-01-24 10:11:02 -0300

    + C:\Users\erick.ssilva20\Desktop\Aula_node\lacos.js

--- lacos.js
+++ lacos.js
@@ -5,3 +5,10 @@ for (let i = 0; i < numero.length; i++) {
     console.log(numero[i]); // Acessando cada elemento do array "numero" usando um loop for
 
 } // Estruturas de repeticao sao usadas para executar um bloco de codigo varias vezes, ate que uma condicao especifica seja atendida. Neste caso, o loop for percorre cada elemento do array "numero" e imprime seu valor no console.
+
+console.log("-----"); // Imprime uma linha de separacao no console para melhor visualizacao dos resultados.
+
+for ( let i = numero.length -1; i >= 0; i--) // Loop for que percorre o array "numero" em ordem reversa, começando do ultimo elemento ate o primeiro.
+{
+    console.log(numero[i]); // Acessando cada elemento do array "numero" em ordem reversa usando um loop for
+} // Isso demonstra como estruturas de repeticao podem ser usadas para iterar sobre arrays em diferentes ordens.
