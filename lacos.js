e2cff58b1f56d044379cbbde64e88fbfe6bd65fe
Erick de souza <ericsouzasilv@gmail.com>
2026-01-24 09:57:51 -0300

    + C:\Users\erick.ssilva20\Desktop\Aula_node\lacos.js

---
+++ lacos.js
@@ -0,0 +1,7 @@
+numero = [1,2,3,4,5]; // Declaracao de um array chamado "numero" que armazena cinco elementos numericos.
+
+
+for (let i = 0; i < numero.length; i++) {
+    console.log(numero[i]); // Acessando cada elemento do array "numero" usando um loop for
+
+} // Estruturas de repeticao sao usadas para executar um bloco de codigo varias vezes, ate que uma condicao especifica seja atendida. Neste caso, o loop for percorre cada elemento do array "numero" e imprime seu valor no console.
