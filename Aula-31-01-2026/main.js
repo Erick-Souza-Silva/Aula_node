8af7c8498631004e7db497243d809d42dfaf7567
Erick de souza <ericsouzasilv@gmail.com>
2026-01-31 11:30:54 -0300

    Atenção os arquivos json e outros podem não ser obrigatorio no demais sistemas

---
+++ Aula-31-01-2026/main.js
@@ -0,0 +1,9 @@
+// Comandaods basicos do NPM
+// npm init -y  -> cria o arquivo package.json com as configurações padrão
+// npm install <nome do pacote> -> instala um pacote e adiciona ao package.json
+
+function main() {
+  console.log('Hello from main.js');
+}
+
+main();
